adedefaae69869582253314b1553707fbde1eb91
miguel perez <maperez.r93@gmail.com>
2023-10-04 20:31:46 -0500

    deploy

--- pamo/settings.py
+++ pamo/settings.py
@@ -81,8 +81,13 @@ WSGI_APPLICATION = 'pamo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD':'GP85Axpkg2N6QalOoigN',
+        'HOST': 'containers-us-west-93.railway.app',
+        'PORT': '7902'
+
     }
 }
 
