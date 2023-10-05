372ca0b678b693398422b36577507c6e7ccca7cf
miguel perez <maperez.r93@gmail.com>
2023-10-04 19:51:24 -0500

    cambios menores

--- pamo/conecctions_shopify.py
+++ pamo/conecctions_shopify.py
@@ -10,8 +10,8 @@ class ConnectionsShopify():
     def __init__(self) -> None:
         self.headers_shopify = {'X-Shopify-Access-Token' : ACCES_TOKEN, 'Content-Type' : 'application/json'}
 
-    def request_graphql(self, query):
-        return requests.post(URL_GRAPHQL, headers=self.headers_shopify, data=json.dumps({"query": query}))
+    def request_graphql(self, query, variables = None ):
+        return requests.post(URL_GRAPHQL, headers=self.headers_shopify, data=json.dumps({"query": query, 'variables': variables}))
 
     def get_rest(self,url):
         return requests.get(url, headers= self.headers_shopify)
