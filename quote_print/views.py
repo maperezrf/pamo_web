8bb955a39469f25630597a32383dbe2c4e1d6b76
miguel perez <maperez.r93@gmail.com>
2023-10-07 15:17:29 -0500

    modificacion plantilla

--- quote_print/views.py
+++ quote_print/views.py
@@ -19,23 +19,24 @@ def index(request):
     return render(request, 'table_draft_orders.html', data)
 
 def print_drafr(request,id):
-    ConnectionsShopify()
-    shopify = ConnectionsShopify()
-    query = GET_DRAFT_ORDER.format(id)
-    response = shopify.request_graphql(query)
-    date_update = datetime.strptime(response.json()['data']['draftOrders']['edges'][0]['node']['updatedAt'][0:10], '%Y-%m-%d')
-    plazo = (date_update + timedelta(days=10)).strftime('%d/%m/%Y')
-    draft = response.json()['data']['draftOrders']['edges'][0]['node']
-    try:
-        res = re.search(r'\[(\d+)\]' ,draft['customer']['metafields']['edges'][0]['node']['value'])
-        num = res.group(1)
-    except:
-        num = None
-    for i in draft['lineItems']['edges']:
-        try:
-            i['sku'] = i['node']['product']['variants']['edges'][0]['node']['sku']
-            i['scr'] = i['node']['product']['images']['edges'][0]['node']['originalSrc']
-        except:
-            pass
-    data = {'info':draft, 'plazo':plazo, 'update': date_update.strftime('%d/%m/%Y'), 'nit':num}
+    # ConnectionsShopify()
+    # shopify = ConnectionsShopify()
+    # query = GET_DRAFT_ORDER.format(id)
+    # response = shopify.request_graphql(query)
+    # date_update = datetime.strptime(response.json()['data']['draftOrders']['edges'][0]['node']['updatedAt'][0:10], '%Y-%m-%d')
+    # plazo = (date_update + timedelta(days=10)).strftime('%d/%m/%Y')
+    # draft = response.json()['data']['draftOrders']['edges'][0]['node']
+    # try:
+    #     res = re.search(r'\[(\d+)\]' ,draft['customer']['metafields']['edges'][0]['node']['value'])
+    #     num = res.group(1)
+    # except:
+    #     num = None
+    # for i in draft['lineItems']['edges']:
+    #     try:
+    #         i['sku'] = i['node']['product']['variants']['edges'][0]['node']['sku']
+    #         i['scr'] = i['node']['product']['images']['edges'][0]['node']['originalSrc']
+    #     except:
+    #         pass
+    # data = {'info':draft, 'plazo':plazo, 'update': date_update.strftime('%d/%m/%Y'), 'nit':num}
+    data = {}
     return render(request, 'print.html', data)
