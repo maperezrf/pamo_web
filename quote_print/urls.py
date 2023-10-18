7efa761829040875f2d4180a710c16009dc0c1b0
miguel perez <maperez.r93@gmail.com>
2023-10-17 22:04:29 -0500

    f trabajando en script de descarga

--- quote_print/urls.py
+++ quote_print/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:id>/print/", views.print_drafr, name="print"),
+    path("<int:id>/download/", views.download_quote, name="download"),
 ]
--- quote_print/views.py
+++ quote_print/views.py
@@ -5,7 +5,11 @@ from pamo.conecctions_shopify import ConnectionsShopify
 from pamo.constants import *
 from pamo.queries import *
 from datetime import timedelta, datetime
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from django.contrib.staticfiles import finders
 import re
+import os
 
 def index(request):
     ConnectionsShopify()
@@ -43,6 +47,73 @@ def print_drafr(request,id):
             pass
     data = {'info':draft, 'plazo':plazo, 'update': date_update.strftime('%d/%m/%Y'), 'nit':num}
     return render(request, 'print.html', data)
+
+def link_callback(uri, rel):
+        """
+        Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+        resources
+        """
+        result = finders.find(uri)
+        if result:
+                if not isinstance(result, (list, tuple)):
+                        result = [result]
+                result = list(os.path.realpath(path) for path in result)
+                path=result[0]
+        else:
+                sUrl = settings.STATIC_URL        # Typically /static/
+                sRoot = settings.STATIC_ROOT      # Typically /home/userX/project_static/
+                mUrl = settings.MEDIA_URL         # Typically /media/
+                mRoot = settings.MEDIA_ROOT       # Typically /home/userX/project_static/media/
+                if uri.startswith(mUrl):
+                        path = os.path.join(mRoot, uri.replace(mUrl, ""))
+                elif uri.startswith(sUrl):
+                        path = os.path.join(sRoot, uri.replace(sUrl, ""))
+                else:
+                        return uri
+
+        # make sure that file exists
+        if not os.path.isfile(path):
+                raise RuntimeError(
+                        'media URI must start with %s or %s' % (sUrl, mUrl)
+                )
+        return path
+
+
+def download_quote(request, id):
+    template_path = 'print.html'
+    ConnectionsShopify()
+    shopify = ConnectionsShopify()
+    query = GET_DRAFT_ORDER.format(id)
+    response = shopify.request_graphql(query)
+    date_update = datetime.strptime(response.json()['data']['draftOrders']['edges'][0]['node']['updatedAt'][0:10], '%Y-%m-%d')
+    plazo = (date_update + timedelta(days=10)).strftime('%d/%m/%Y')
+    draft = response.json()['data']['draftOrders']['edges'][0]['node']
+    try:
+        res = re.search(r'\[(\d+)\]' ,draft['customer']['metafields']['edges'][0]['node']['value'])
+        num = res.group(1)
+    except:
+        num = None
+    for i in draft['lineItems']['edges']:
+        try:
+            i['sku'] = i['node']['product']['variants']['edges'][0]['node']['sku']
+            i['scr'] = i['node']['product']['images']['edges'][0]['node']['originalSrc']
+        except:
+            pass
+    data = {'info':draft, 'plazo':plazo, 'update': date_update.strftime('%d/%m/%Y'), 'nit':num}
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    print('por aqui si pasa')
+    html = template.render(data)
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response, link_callback=link_callback)
+    # if error then show some funny view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
 
 def make_json(res):
     for i in range(len(res)):
