c5650b12c3be3c8ce7eef11d8e9097ce6b3d69cd
Miguel Angel Perez Roa <maperezr@falabella.com.co>
2023-12-14 18:40:17 -0500

    fixed media

--- pamo/urls.py
+++ pamo/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('pamo_bots/', include("pamo_bots.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
