85c06a00f3685066031bca6520f287badb04261b
miguel perez <maperez.r@hotmail.com>
2026-06-10 07:03:34 -0500

    webhooks

--- pamo/connections_envia.py
+++ pamo/connections_envia.py
@@ -25,13 +25,16 @@ class EnviaConnection():
         count+=10
         print(traking_numbers[count:count+10])
         response = requests.request("POST", URL_ENVIA_TRAKING, headers=self.headers, data=payload)
-        data =response.json()['data']
-        dic_data = {i['trackingNumber']:i['status'] for i in data}
-        objs = OrdersShopify.objects.filter(tracking_number__in=dic_data.keys())
-        for obj in objs:
-          status = dic_data[obj.tracking_number]
-          obj.tracking_status = dic_data[obj.tracking_number]
-          if status == 'Delivered':
-            obj.in_transit = True
-          obj.save()
+        try:
+          data =response.json()['data']
+          dic_data = {i['trackingNumber']:i['status'] for i in data}
+          objs = OrdersShopify.objects.filter(tracking_number__in=dic_data.keys())
+          for obj in objs:
+            status = dic_data[obj.tracking_number]
+            obj.tracking_status = dic_data[obj.tracking_number]
+            if status == 'Delivered':
+              obj.in_transit = True
+            obj.save()
+        except Exception as e:
+          print(f'error al tratar de traer las ordenes {e}')
     print(total_traking)
